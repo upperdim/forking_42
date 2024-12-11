21e0bbfebf31c4759d1b1eb28e91432f4f236ac3
upperdim <45048352+upperdim@users.noreply.github.com>
2024-12-11 11:05:38 +0100

    initial commit

---
+++ build.sh
@@ -0,0 +1,1 @@
+gcc -O0 -Wall -Wextra -Werror main.c -o decoder -lpthread
--- main.c
+++ main.c
@@ -4,6 +4,18 @@
 #include <fcntl.h>
 #include <stdio.h>
 
+
+#include <pthread.h>
+#include <immintrin.h>
+#include <sys/stat.h>
+// #include <sys/mmap.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
 typedef char i8;
 typedef unsigned char u8;
 typedef unsigned short u16;
@@ -14,8 +26,7 @@ typedef unsigned long u64;
 #define PRINT_ERROR(cstring) write(STDERR_FILENO, cstring, sizeof(cstring) - 1)
 
 #pragma pack(1)
-struct bmp_header
-{
+struct bmp_header {
 	// Note: header
 	i8  signature[2]; // should equal to "BM"
 	u32 file_size;
@@ -33,42 +44,55 @@ struct bmp_header
 	// Note: there are more stuff there but it is not important here
 };
 
-struct file_content
-{
+struct file_content {
 	i8*   data;
 	u32   size;
 };
 
-struct file_content   read_entire_file(char* filename)
-{
+struct file_content   read_entire_file(char* filename) {
 	char* file_data = 0;
 	unsigned long	file_size = 0;
+
 	int input_file_fd = open(filename, O_RDONLY);
-	if (input_file_fd >= 0)
-	{
-		struct stat input_file_stat = {0};
-		stat(filename, &input_file_stat);
-		file_size = input_file_stat.st_size;
-		file_data = mmap(0, file_size, PROT_READ | PROT_WRITE, MAP_PRIVATE, input_file_fd, 0);
-		close(input_file_fd);
+	if (input_file_fd < 0) {
+		return (struct file_content){file_data, file_size};
 	}
+
+	struct stat input_file_stat = {0};
+	stat(filename, &input_file_stat);
+	file_size = input_file_stat.st_size;
+	file_data = mmap(0, file_size, PROT_READ | PROT_WRITE, MAP_PRIVATE, input_file_fd, 0);
+	
+	close(input_file_fd);
 	return (struct file_content){file_data, file_size};
 }
 
-int main(int argc, char** argv)
-{
-	if (argc != 2)
-	{
+int main(int argc, char** argv) {
+	if (argc != 2) {
 		PRINT_ERROR("Usage: decode <input_filename>\n");
 		return 1;
 	}
+
 	struct file_content file_content = read_entire_file(argv[1]);
-	if (file_content.data == NULL)
-	{
+	if (file_content.data == NULL) {
 		PRINT_ERROR("Failed to read file\n");
 		return 1;
 	}
+
 	struct bmp_header* header = (struct bmp_header*) file_content.data;
-	printf("signature: %.2s\nfile_size: %u\ndata_offset: %u\ninfo_header_size: %u\nwidth: %u\nheight: %u\nplanes: %i\nbit_per_px: %i\ncompression_type: %u\ncompression_size: %u\n", header->signature, header->file_size, header->data_offset, header->info_header_size, header->width, header->height, header->number_of_planes, header->bit_per_pixel, header->compression_type, header->compressed_image_size);
+
+	printf("signature: %.2s\nfile_size: %u\ndata_offset: %u\ninfo_header_size: %u\nwidth: %u\nheight: %u\nplanes: %i\nbit_per_px: %i\ncompression_type: %u\ncompression_size: %u\n", 
+		header->signature, 
+		header->file_size, 
+		header->data_offset, 
+		header->info_header_size, 
+		header->width, 
+		header->height, 
+		header->number_of_planes, 
+		header->bit_per_pixel, 
+		header->compression_type, 
+		header->compressed_image_size
+	);
+	
 	return 0;
 }
